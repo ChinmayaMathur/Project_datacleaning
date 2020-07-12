7e6dfd8a09d757129d4a62da1a224e2d34327189
ChinmayaMathur <66267059+ChinmayaMathur@users.noreply.github.com>
2020-07-12 23:59:19 +0530

    Add files via upload

--- run_analysis.R
+++ run_analysis.R
@@ -1,47 +1,40 @@
-#Part1
+#Part1 (Merge the dataset)
 X_train <- read.table("X_train.txt")
 y_train <- read.table("y_train.txt")
+subject1 <- read.table("subject_train.txt")
+
 
 X_test <- read.table("X_test.txt")
 y_test <- read.table("y_test.txt")
-
-subject1 <- read.table("subject_train.txt")
 subject2<- read.table("subject_test.txt")
 
 features <- read.table("features.txt")
+
 
 combine1 <- cbind(X_train,y_train)
 combine2 <- cbind(X_test,y_test)
 combine3 <- cbind(combine1,subject1)
 combine4 <- cbind(combine2,subject2)
-combine5 <- rbind(X_test,X_train)
 combine_X <- rbind(combine3,combine4)
 
 
 
-#Part2
+#Part2 (extract the mean and standard deviation columns)
 dataset <- combine_X[c(1:6,41:46,81:86,121:126,161:166,201,202,214,215,227,228,240,241,253,254,
-                      266:271,345:350,424:429,503,504,516,517,529,530,542,543,555:561,562,563)]
+                      266:271,345:350,424:429,503,504,516,517,529,530,542,543,562,563)]
 
 
 
-#Part3
-say <- function(x) {
-  for (i in 1:6) {
-  if(dataset$activity==1) {"1"="WALKING"}
-  if(dataset$activity==2) {"2"="WALKING_UPSTAIRS"}
-  if(dataset$activity==3) {"3"="WALKING_DOWNSTAIRS"}
-  if(dataset$activity==4) {"4"="SITTING"}
-  if(dataset$activity==5) {"5"="STANDING"}
-  else {"6"="LAYING"}}}
-
- 
-dataset <- mutate(dataset, activity = "WALKING")
+#Part3 (change the activity names from numeric to text)
+activity_lable <- read.table("activity_labels.txt")
+dataset <- merge(dataset, activity_lable, by.x = "activity", by.y = "V1")
+dataset$activity<- NULL
+dataset <- select(dataset, subject, everything())
+dataset <- select(dataset, V2, everything())
 
 
-
-#Part4
-colnames(dataset) = c("tBodyAcc-mean()-X","tBodyAcc-mean()-Y","tBodyAcc-mean()-Z","tBodyAcc-std()-X",
+#Part4 (name all the vairables)
+colnames(dataset) = c("activity","subject","tBodyAcc-mean()-X","tBodyAcc-mean()-Y","tBodyAcc-mean()-Z","tBodyAcc-std()-X",
                      "tBodyAcc-std()-Y","tBodyAcc-std()-Z","tGravityAcc-mean()-X","tGravityAcc-mean()-Y",
                      "tGravityAcc-mean()-Z","tGravityAcc-std()-X","tGravityAcc-std()-Y","tGravityAcc-std()-Z",
                      "tBodyAccJerk-mean()-X","tBodyAccJerk-mean()-Y","tBodyAccJerk-mean()-Z","tBodyAccJerk-std()-X",
@@ -57,17 +50,14 @@ colnames(dataset) = c("tBodyAcc-mean()-X","tBodyAcc-mean()-Y","tBodyAcc-mean()-Z
                      "fBodyGyro-mean()-Y","fBodyGyro-mean()-Z","fBodyGyro-std()-X","fBodyGyro-std()-Y",
                      "fBodyGyro-std()-Z","fBodyAccMag-mean()","fBodyAccMag-std()","fBodyBodyAccJerkMag-mean()",
                      "fBodyBodyAccJerkMag-std()","fBodyBodyGyroMag-mean()","fBodyBodyGyroMag-std()",
-                     "fBodyBodyGyroJerkMag-mean()","fBodyBodyGyroJerkMag-std()","angle(tBodyAccMean,gravity)",
-                     "angle(tBodyAccJerkMean),gravityMean","angle(tBodyGyroMean,gravityMean)",
-                     "angle(tBodyGyroJerkMean,gravityMean)","angle(X,gravityMean)","angle(Y,gravityMean)",
-                     "angle(Z,gravityMean)","activity","subject")
+                     "fBodyBodyGyroJerkMag-mean()","fBodyBodyGyroJerkMag-std()")
 
 
 
-#Part5
+#Part5 (create a new dataset with average of all variables for each subject and activity)
+data <- group_by(dataset, subject, activity) 
+dataset2 <- summarise_each(data,mean)
 
-
-
-write.table(dataset,"dataset.txt",row.names = FALSE)
-data<- read.table("dataset.txt")
-View(data)
+write.table(dataset2,"data.txt",row.names = FALSE)
+data1<- read.table("data.txt")
+View(data1)
